950da417a8c03b9917d4bd54b82ed6878f45d5dd
gunadi wibowo <gunadi.wibowo@dto.kemkes.go.id>
2022-09-10 07:48:58 +0700

    pelajaran dasar

---
+++ a.variable/variable_1dasar.py
@@ -0,0 +1,33 @@
+print("A. CARA BIASA-----------------------------")
+nama = "wati"
+umur = 25
+
+print(type(nama))
+print(nama)
+print(type(nama), nama)
+
+print(type(umur))
+print(umur)
+
+print("B. MULTIPLE VAR & VALUE-----------------------------")
+
+sepatu, baju, celana = 37, "M", 23
+print(sepatu)
+print(baju)
+print(celana)
+
+print("C. MULTIPLE VAR & SINGLE VALUE-----------------------------")
+sepatu = baju = celana = 37
+print(sepatu)
+print(baju)
+print(celana)
+
+print("D. UNPACK COLLECTION-----------------------------")
+
+ukuran = [37, "M", 23]
+sepatu, baju, celana = ukuran
+print(sepatu)
+print(baju)
+print(celana)
+
+print("-----------------------------")
---
+++ a.variable/variable_2operasional.py
@@ -0,0 +1,6 @@
+nama = "budi"
+kegiatan = "bersepeda"
+tempat = "ke sekolah"
+
+print(nama, kegiatan, tempat)
+print(nama +" "+ kegiatan +" "+ tempat)
